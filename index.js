3f2b3878fccd7e41d7f4e4de241e83d4ce313654
lromina <lromina01@gmail.com>
2024-05-01 01:19:24 -0300

    Agregacion de la extension cors para leer en dos puertos

--- index.js
+++ index.js
@@ -1,80 +1,20 @@
 //costante para llamar a express y traer la galeria
 const express = require ('express');
 
+const cors = require ('cors')
+
 //crear const para activar a express y sus funcionalidades
 const app = express();
 
 //crear el puerto por defecto se usa el 3000
 const PORT = 3001;
 
+app.use(cors());
 //invocamos a express para usarlo
 app.use(express.json());
 
-//llamar a un servicio para traer lista de usuario
-
-// let pokemon = [
-//     {nombre: 'pikachu',
-//     id: 1,
-//     especie: 'electrico'
-//     },
-
-//     {nombre: 'charizer',
-//     id: 2,
-//     especie: 'fuego'
-//     },
-
-//     {nombre: 'bulbazur',
-//     id: 3,
-//     especie: 'planta'
-//     }
-// ];
-
-// app.get('/all',(req,res) => {
-//     res.json(pokemon)
-// });
-
-// //es como crear el home
-// app.get('/', (req,res)=>{
-//     res.send('hola estamos en el back de node')
-// });
-
-// //para que el puerto se activo tengo que escucharlo
-
-
-
-// //buscamos datos por ID
-
-// app.get('/all/:id', (req,res)=>{
-//     const allPoke = parseInt(req.params.id); //extrae el id colocado en la ruta
-//     const pokeId = pokemon.find(poke => poke.id === allPoke); //compara con el id del arreglo pokemon
-
-//     if (pokeId){
-//         res.json(pokeId)
-//     }else{
-//         res.status(404).json({mensaje: 'pokemon no encontrado'})
-//     }
-// });
-
-
-// //para crear infomacion
-
-// app.post('/new' , (req,res) => {
-//     const nuevoPokemon = req.body; //asigna a la variable lo que hay en el body
-//     //si no existe cargado nada o nombre
-//     if(!nuevoPokemon || !nuevoPokemon.nombre || !nuevoPokemon.id){
-//         return res.status(400).json({mensaje: 'Datos invalidos'});
-//     }
-//     const nuevoId = pokemon.find (e => e.id === nuevoId.id)
-//     const nuevoNombre = pokemon.find (e => e.nombre === nuevoPokemon.nombre)
-//     if(nuevoNombre && nuevoId){
-//         return res.status(400).json({mensaje: 'Datos ya existente'})
-//     }else{
-
-//     nuevoPokemon.id = pokemon.length + 1;
-//     pokemon.push(nuevoPokemon); //agrega los datos que cargamos
-//     res.status(201).json(nuevoPokemon);
-// }
-// })
+//luego de instalar fs "npm i fs" que nos permitira trabajar con el archivo json lo invocamos
+const fs = require('fs');
 
 //Middleware para parsear el cuerpo de las solicitudes
 const bodyParser = require('body-parser');
@@ -83,9 +23,6 @@ app.use(bodyParser.json()); // Usa el middleware body-parser para analizar el cu
 
 //creamos una constante para traer el archivo creado
 const allUser = 'user.json';
-
-//luego de instalar fs "npm i fs" que nos permitira trabajar con el archivo json lo invocamos
-const fs = require('fs');
 
 //Funcion de Autenticacion de usuarios - libreria Body Parser
 const autheticateUser = (req, res, next)=>{
@@ -103,6 +40,8 @@ const autheticateUser = (req, res, next)=>{
 
             const usuarioEncontrado = objJSON.usuarios.find(u => u.user === user && u.clave === clave);
             //creamos una constante para guardar los datos del archivo json
+
+            console.log('usuario encontrado', usuarioEncontrado);
             if (usuarioEncontrado){
                 req.autheticateUser = user;
                 next();
@@ -130,12 +69,18 @@ fs.readFile(allUser,'utf-8',(err,data) => {
     try{
         const objJSON = JSON.parse(data) 
         const{user,clave} = req.body
+
+        console.log('encontre este usuario', user)
+
         const usuarioEncontrado = objJSON.usuarios.find(u => u.user === user && u.clave === clave);
+
+        console.log('objJSON', objJSON);
+        console.log('usuario encontrado', usuarioEncontrado)
         //creamos una constante para guardar los datos del archivo json
-        if (usuarioEncontrado){
-            res.send('bienvenido')
+        if (usuarioEncontrado !== undefined){
+            res.json('bienvenido')
         }else{
-            res.send('usuario no encontrado')
+            res.json('usuario no encontrado')
         }        //mostramos en postman esos datos. 
 
     } catch (parseError){
